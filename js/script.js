b19f68b6757728097810c2567398c2224b7c331d
Roman Melnychuk <roman4iek02@gmail.com>
2022-09-05 21:33:20 +0300

    preview

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// SLIDER
+const swiper = new Swiper('.slider-preview', {
+    // Optional parameters
+    loop: true,
+    speed: 1000,
+  
+    
+    autoplay: {
+      delay: 2700,
+    }
+  });
